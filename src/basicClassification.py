2ca685349266b72ae2a43724988fdc6e2f175c7e
yann dardot <yann@donkeycode.com>
2018-08-28 20:35:26 +0200

    fix nodes

--- src/basicClassification.py
+++ src/basicClassification.py
@@ -19,9 +19,8 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(15, activation=tf.nn.softmax)
+    keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
-
 
 
 model.compile(optimizer=tf.train.AdamOptimizer(), 
